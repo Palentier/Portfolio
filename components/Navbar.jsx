e92d3dbc8a5731ae19d56c818a147176c51a19a8
Palentier <palentier.business@gmail.com>
2022-11-13 18:22:40 -0600

    Starting developement -
    Getting started with the navbar.

---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return(
+    <nav className=''>
+        <div className="navbar-left font-bold">
+            Ipsum pariatur
+        </div>
+    </nav>
+  )
+}
