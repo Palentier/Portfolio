fa2db555def3584cbd1596ed008c46c7494e38b9
Palentier <palentier.business@gmail.com>
2022-11-14 17:19:33 -0600

    Finished landing page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,36 @@
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import Image from 'next/image';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className=''>
-      <Navbar/>
-        <div className="landing-section">
-          <div className="left-landing font-black text-4xl">
-            Hello, I&apos;m Palentier. 
-          </div>
-          <p>
-          I&apos;m a front-end developer based in the United States.
-          </p>
+    <div className="">
+      <Navbar />
+      <div className="landing-section">
+        <div className="left-landing font-black text-4xl">
+          <h1> Hello, I&apos;m Palentier. </h1>
+          <p>I&apos;m a front-end developer based in the United States.</p>
           <div className="flex">
-          <Image width={40} height={40} src="/icons8-discord-new-50.png" />
-          <Image width={40} height={40} src="/icons8-github-64.png" />
-          <Image width={40} height={40} src="/icons8-twitter-30.png" />
+            <Link href="https://discord.gg/k2FEGjxqN2">
+              <Image width={40} height={40} src="/icons8-discord-new-50.png" />
+            </Link>
+            <Link href="https://github.com/palentier">
+              <Image width={40} height={40} src="/icons8-github-64.png" />
+            </Link>
+            <Link href="https://twitter.com/palentier_">
+              <Image width={40} height={40} src="/icons8-twitter-30.png" />
+            </Link>
           </div>
         </div>
+        <div className="right-landing ">
+          <Image
+            width={100}
+            height={100}
+            src="/undraw_code_typing_re_p8b9.svg"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
