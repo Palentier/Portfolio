223e9e7c01913a2a45d9e7efc1fe1044b9ff3454
Palentier <palentier.business@gmail.com>
2022-12-02 18:17:04 -0600

    added grayscale to language images

--- containers/Languages.jsx
+++ containers/Languages.jsx
@@ -1,27 +1,27 @@
 import Image from 'next/image'
 
-import { figma, git, javascript, next, react, tailwind } from '../imports/langs/langs'
+import { figma, git, javascript, mongo, react, tailwind } from '../imports/langs/langs'
 
 
 export default function Langs() {
     return(
         <div className="plnt__langs section__padding">
-            <div>
+            <div className='plnt__langs-effect'>
             <Image src={figma} width={65} height={65}/>
             </div>
-            <div>
+            <div className='plnt__langs-effect'>
             <Image src={git} width={65} height={65}/>
             </div>
-            <div>
+            <div className='plnt__langs-effect'>
             <Image src={javascript} width={65} height={65}/>
             </div>
-            <div>
-            <Image src={next} width={65} height={65}/>
+            <div className='plnt__langs-effect'>
+            <Image src={mongo} width={65} height={65}/>
             </div>
-            <div>
+            <div className='plnt__langs-effect'>
             <Image src={react} width={65} height={65}/>
             </div>
-            <div>
+            <div className='plnt__langs-effect'>
             <Image src={tailwind} width={65} height={65}/>
             </div>
         </div>
--- imports/langs/langs.js
+++ imports/langs/langs.js
@@ -1,7 +1,7 @@
 import figma from '../assets/lang-assests/icons8-figma-100.png';
 import git from '../assets/lang-assests/icons8-git-100.png';
 import javascript from '../assets/lang-assests/icons8-javascript-100.png';
-import next from '../assets/lang-assests/icons8-next.js-100.png';
+import mongo from '../assets/lang-assests/icons8-mongodb-100.png';
 import react from '../assets/lang-assests/icons8-react-100.png';
 import tailwind from '../assets/lang-assests/icons8-tailwindcss-100.png'
 
@@ -10,7 +10,7 @@ export {
     figma,
     git,
     javascript,
-    next,
+    mongo,
     react,
     tailwind,
 };
